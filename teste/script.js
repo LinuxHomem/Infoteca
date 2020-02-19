34f4885130268a35b4f464e94ffe1a797635a300
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2020-02-18 21:25:45 -0300

    Pegando valores da API e guardando em uma lista

--- teste/script.js
+++ teste/script.js
@@ -2,6 +2,7 @@ const key = "AIzaSyA9bmUVTuY0jMbF6CCimfl1T2FjxkbNk08";
 
 var input = document.querySelector("#campo-busca");
 var botao = document.querySelector("#botao");
+var divLivros = document.querySelector(".lista-livros");
 
 botao.addEventListener("click", function () {
     var pesquisa = input.value;
@@ -10,20 +11,37 @@ botao.addEventListener("click", function () {
     axios.get(url)
         .then(function (response){
             let data = response.data;
+            divLivros.innerHTML = "";
 
             for (let i = 0; i <= 9; i++) {
                 
                 let items = data.items[i];
                 let volume = items.volumeInfo;
-                
-                let title = volume.title;
-                let desc = volume.description;
-                let publish = volume.publishedDate;
+                let imgCaminho = volume.imageLinks;
 
-                let img = volume.imageLinks;
-                let imgurl = img.thumbnail;
+                let livro = {
+                    titulo: volume.title,
+                    descricao: volume.description,
+                    dataPublicacao: volume.publishedDate,
+                    imgUrl: imgCaminho.thumbnail
+                }
 
-                console.log(imgurl);
+                // Inserindo livros
+                let div = document.createElement("div");
+                div.classList.add("livro");
+
+                // Titulo
+                let h1 = document.createElement("h1");
+                let textoH1 = document.createTextNode(livro.titulo);
+                h1.appendChild(textoH1);
+                div.appendChild(h1);
+
+                // Imagem
+                let img = document.createElement("img");
+                img.src = livro.imgUrl;
+                div.appendChild(img);
+
+                divLivros.appendChild(div);
             }
         })
         .catch(function (erro) {
