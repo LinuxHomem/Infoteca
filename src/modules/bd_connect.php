0dc8790b06a36430751ac73886355cfb48800fed
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2020-02-19 18:06:17 -0300

    Deixamdo a conexão em UTF-8

--- src/modules/bd_connect.php
+++ src/modules/bd_connect.php
@@ -7,4 +7,9 @@
 
   $connect = mysqli_connect($servername,$username,$password,$db_name);
 
+  // Deixamdo conexão em UTF-8
+  mysql_query("SET NAMES 'utf8'");
+  mysql_query('SET character_set_connection=utf8');
+  mysql_query('SET character_set_client=utf8');
+  mysql_query('SET character_set_results=utf8');
  ?>
