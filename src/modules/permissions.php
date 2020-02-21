bffd2a03f5ddccf20acbf61486e4cb06e825500c
unknown <vinibr115@gmail.com>
2020-02-21 11:44:13 -0300

    Adição de mais níveis de permissão

--- src/modules/permissions.php
+++ src/modules/permissions.php
@@ -7,13 +7,39 @@
     $sql = "SELECT * FROM usuarios WHERE id = '$id'";
     $dados = mysqli_fetch_array(mysqli_query($connect,$sql));
     global $dados;
+
     function cadastro(){
-      for($count=2;$count<5;$count++){
-        if($GLOBALS['dados']["distincao"] == $count){
+        if($GLOBALS['dados']["distincao"] != 1){
           header('Location:index.php');
         }
       }
+
+    function emprestimo(){
+      if($GLOBALS['dados']["distincao"] == 5){
+        // BLOAQUEAR EMPRESTIMO
+      }
     }
+
+    function devolucao(){
+      for($count=3;$count<5;$count++){
+        if($GLOBALS['dados']["distincao"] == $count){
+          // BLOAQUEAR DEVOLUÇÃO
+        }
+      }
+    }
+
+    function livro(){
+      if($GLOBALS['dados']["distincao"] != 1){
+        // BLOAQUEAR ADIÇÃO/SUBTRAÇÃO DE LIVROS
+      }
+    }
+
+    function livro(){
+      if($GLOBALS['dados']["distincao"] != 1){
+        // BLOAQUEAR ADIÇÃO/SUBTRAÇÃO DE LIVROS
+      }
+    }
+
   }else{
     header('Location:index.php');
   }
